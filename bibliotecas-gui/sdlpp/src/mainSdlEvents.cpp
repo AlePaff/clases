fcff7c8c7862a4bbdfd83e25850fe6e393d4b3ec
Federico Gomez Peter <fedemgp@gmail.com>
2021-12-04 21:50:56 -0300

    Agrego un ejemplo con texto musica y animaciones en sdl2pp para usarlo como ejemplo para archivo de supresion

--- bibliotecas-gui/sdlpp/src/mainSdlEvents.cpp
+++ bibliotecas-gui/sdlpp/src/mainSdlEvents.cpp
@@ -2,23 +2,40 @@
 #include <iostream>
 #include <exception>
 #include <unistd.h>
+#include <vector>
 
 #include "Player.h"
+#include <SDL2pp/SDLMixer.hh>
+#include <SDL2pp/Mixer.hh>
+#include <SDL2pp/Chunk.hh>
+#include <SDL2pp/SDLTTF.hh>
+#include <SDL2pp/Font.hh>
 
 static bool handleEvents(Player &player);
-static void render(SDL2pp::Renderer &renderer, Player &player);
+static void render(SDL2pp::Renderer &renderer, Player &player,std::vector<SDL2pp::Texture> &textures);
 static void update(Player &player, float dt);
 
 int main(int argc, char** argv){
     try {
         // Inicializo biblioteca de SDL
         SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+        SDL2pp::SDLTTF ttf;
         // Creo una ventana dinamica con titulo "Hello world"
         SDL2pp::Window window("Hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
             800, 600, SDL_WINDOW_RESIZABLE);
+        SDL2pp::Mixer mixer(MIX_DEFAULT_FREQUENCY, MIX_DEFAULT_FORMAT, MIX_DEFAULT_CHANNELS, 4096);
 
+        SDL2pp::Chunk sound("assets/MurderTrain.wav");
+        mixer.PlayChannel(-1, sound);
         // Creo renderer
         SDL2pp::Renderer renderer(window, -1, SDL_RENDERER_ACCELERATED);
+        
+        SDL2pp::Font font("./assets/fefo.TTF", 100);
+
+        std::vector<SDL2pp::Texture> textures;
+        textures.emplace_back(renderer,
+            font.RenderText_Solid("Hello, world! (solid mode)", SDL_Color{0xff,0xff,0,0xff})
+        );
 
         // Usar factory
         SDL2pp::Texture im(renderer, 
@@ -31,7 +48,7 @@ int main(int argc, char** argv){
         while (running) {
             running = handleEvents(player);
             update(player, FRAME_RATE);
-            render(renderer, player);
+            render(renderer, player, textures);
             // la cantidad de segundos que debo dormir se debe ajustar en función
             // de la cantidad de tiempo que demoró el handleEvents y el render
             usleep(FRAME_RATE);
@@ -89,9 +106,14 @@ static bool handleEvents(Player &player) {
     return true;
 }
 
-static void render(SDL2pp::Renderer &renderer, Player &player) {
+static void render(SDL2pp::Renderer &renderer, Player &player, std::vector<SDL2pp::Texture> &textures) {
+    renderer.SetDrawColor(0xFF,0xFF,0xFF,0xFF);
     renderer.Clear();
     player.render(renderer);
+    for (auto& texture: textures) {
+        renderer.Copy(texture, SDL2pp::NullOpt, SDL2pp::Rect(50, 50, texture.GetWidth(), texture.GetHeight()));
+    }
+
     renderer.Present();
 }
 
