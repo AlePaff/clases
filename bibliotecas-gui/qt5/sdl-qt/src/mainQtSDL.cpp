dd4e55c96b795859870ae2f19ccab35cfcb9fe11
Federico Gomez <fedemgp@gmail.com>
2022-05-14 19:07:39 -0300

    Arreglo ejemplo de qt + sdl

--- bibliotecas-gui/qt5/sdl-qt/src/mainQtSDL.cpp
+++ bibliotecas-gui/qt5/sdl-qt/src/mainQtSDL.cpp
@@ -1,38 +1,45 @@
 #include <QApplication>
 #include <iostream>
 #include "MainWindow.h"
-#include "SdlWindow.h"
-#include "SdlImage.h"
-
-class SDL_Renderer;
-class SDL_Texture;
+#include "Gameloop.h"
+#include <SDL2pp/SDL2pp.hh>
 
 int main(int argc, char *argv[]) {
-    // Clase que contiene el loop principal
-    QApplication app(argc, argv);
-    // Instancio la ventana principal
-    MainWindow window;
-    window.show();
-    unsigned int winId = window.getWinId();
-    std::cout << "WinId: " << winId << std::endl;
-    if (!winId) {
-        return -1;
-    }
-    // HOLY SHIT, SDL!
-    SdlWindow sdlWindow((void*) &winId);
-    sdlWindow.fill(0x33,0x66,0x99, 0xFF);
-    sdlWindow.render();
-    SDL_Renderer* renderer = sdlWindow.getRenderer();
     try {
-        SdlImage im("cat.gif", renderer);
-        Area srcArea(0, 0, 300, 300);
-        Area destArea(50, 50, 300, 300);
-        sdlWindow.renderTexture(im.getTexture(), srcArea, destArea);
-        sdlWindow.render();
-    } catch (const char* e) {
-        std::cout << "Reventó SDL: " << e << std::endl;
-    }
-    // Arranca el loop de la UI
-    return app.exec();
-}
+        // Clase que contiene el loop principal
+        QApplication app(argc, argv);
+        // Instancio la ventana principal
+        MainWindow window;
+        window.show();
+        /*
+         * QApplication::exec inicia el event loop y se queda esperando que finalice la aplicación. Cuando el usuario
+         * realice una acción que haga un exit de QT, entonces esta función retorna (con el código de error
+         * si lo hubo) y entonces se podrá crear la ventana de SDL.
+         */
+        // exec es una función bloqueante, cuando QT finaliza su ejecución, se realiza un return con el codigo de error
+        if (app.exec()) {
+            throw std::runtime_error("La aplicación QT finalizó de forma incorrecta");
+        }
 
+
+        SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+        SDL2pp::Window sdlWindow("Hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                 800, 600,
+                                 SDL_WINDOW_RESIZABLE);
+
+        // Creo renderer
+        SDL2pp::Renderer renderer(sdlWindow, -1, SDL_RENDERER_SOFTWARE);
+
+        // Encapsular en un repositorio de texturas para no crear multiples veces la misma textura
+        SDL2pp::Texture im(renderer,
+                           SDL2pp::Surface("assets/soldier2.png").SetColorKey(true, 0));
+
+        Gameloop gameloop(renderer, im);
+        gameloop.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Error desconocido en la función main" << std::endl;
+    }
+    return 0;
+}
--- bibliotecas-gui/qt5/sdl-qt/src/mainSDL.cpp
+++ bibliotecas-gui/qt5/sdl-qt/src/mainSDL.cpp
@@ -1,15 +1,26 @@
-#include <SDL2/SDL.h>
-#include "SdlWindow.h"
-#include "SdlImage.h"
+#include <SDL2pp/SDL2pp.hh>
+#include "Gameloop.h"
 
 int main(int argc, char** argv){
-	SdlWindow window(400,400);
-	window.fill();
-	SDL_Renderer* renderer = window.getRenderer();
-	SdlImage im("cat.gif", renderer);
-    Area srcArea(0, 0, 300, 300);
-	Area destArea(50, 50, 300, 300);
-	window.renderTexture(im.getTexture(), srcArea, destArea);
-	window.render();
-	SDL_Delay(3000);
+    try {
+        SDL2pp::SDL sdl(SDL_INIT_VIDEO);
+        SDL2pp::Window sdlWindow("Hello world", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+                                 800, 600,
+                                 SDL_WINDOW_RESIZABLE);
+
+        // Creo renderer
+        SDL2pp::Renderer renderer(sdlWindow, -1, SDL_RENDERER_SOFTWARE);
+
+        // Encapsular en un repositorio de texturas para no crear multiples veces la misma textura
+        SDL2pp::Texture im(renderer,
+                           SDL2pp::Surface("assets/soldier2.png").SetColorKey(true, 0));
+
+        Gameloop gameloop(renderer, im);
+        gameloop.run();
+    } catch (const std::exception &e) {
+        std::cerr << e.what() << std::endl;
+    } catch (...) {
+        std::cerr << "Error desconocido en la función main" << std::endl;
+    }
+
 }
